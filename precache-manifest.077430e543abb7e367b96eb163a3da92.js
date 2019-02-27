340d2fca471d21f9f1ac6d963706f784af3e68a9
zce <w@zce.me>
2019-02-27 20:57:05 +0800

    Updates

---
+++ precache-manifest.077430e543abb7e367b96eb163a3da92.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bfe3d07526391a69e1b4",
+    "url": "/zce/todos/static/js/runtime~main.bfe3d075.js"
+  },
+  {
+    "revision": "ded1c11df47b8cb60410",
+    "url": "/zce/todos/static/js/main.ded1c11d.chunk.js"
+  },
+  {
+    "revision": "413dee5f4c45866e7f25",
+    "url": "/zce/todos/static/js/2.413dee5f.chunk.js"
+  },
+  {
+    "revision": "413dee5f4c45866e7f25",
+    "url": "/zce/todos/static/css/2.ba601e64.chunk.css"
+  },
+  {
+    "revision": "e20627d7c3adc2c8a9274d4bccb05d67",
+    "url": "/zce/todos/index.html"
+  }
+];
