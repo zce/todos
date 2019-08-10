eeabdeaf89637be9c55059aedcf5d9a7673a7f33
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-10 11:20:08 +0000

    Deploy zce/todos to github.com/zce/todos.git:gh-pages

---
+++ precache-manifest.906f85874a34a6a72db32370dcfd476e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e66638e346174735b040557499b8664e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "19356cf7a873d8fc9719",
+    "url": "./static/css/2.8dd801da.chunk.css"
+  },
+  {
+    "revision": "791148779ce29b8026a2",
+    "url": "./static/css/main.19b133c6.chunk.css"
+  },
+  {
+    "revision": "19356cf7a873d8fc9719",
+    "url": "./static/js/2.06178232.chunk.js"
+  },
+  {
+    "revision": "791148779ce29b8026a2",
+    "url": "./static/js/main.99d3879e.chunk.js"
+  },
+  {
+    "revision": "04ca8a980f44b424a28f",
+    "url": "./static/js/runtime~main.ab2345a4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,10 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.bfe606772d6633947440540d7aaf2708.js"
+  "./precache-manifest.906f85874a34a6a72db32370dcfd476e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
