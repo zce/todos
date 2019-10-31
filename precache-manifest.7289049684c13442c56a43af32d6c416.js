0f724ecba1d5435b74a089e3bf2951cd4f3a6d50
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-31 13:46:18 +0000

    Deploy zce/todos to github.com/zce/todos.git:gh-pages

---
+++ precache-manifest.7289049684c13442c56a43af32d6c416.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf3e82520aaacbab432a127500f91280",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9b3999fa4ebf5465ccd6",
+    "url": "./static/css/2.8f3b27ef.chunk.css"
+  },
+  {
+    "revision": "7c4dce86fc685d19ee7c",
+    "url": "./static/css/main.19b133c6.chunk.css"
+  },
+  {
+    "revision": "9b3999fa4ebf5465ccd6",
+    "url": "./static/js/2.d7dad587.chunk.js"
+  },
+  {
+    "revision": "7c4dce86fc685d19ee7c",
+    "url": "./static/js/main.03aaf109.chunk.js"
+  },
+  {
+    "revision": "d728601fe8e8d2d1abaf",
+    "url": "./static/js/runtime-main.7c434eef.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.639742057ba485138472a068e9c47d68.js"
+  "./precache-manifest.7289049684c13442c56a43af32d6c416.js"
 );
 
 self.addEventListener('message', (event) => {
