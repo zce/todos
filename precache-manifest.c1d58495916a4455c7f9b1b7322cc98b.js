1d81389503871d5de88fae860d35c670129d82d6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-28 02:22:25 +0000

    Deploy zce/todos to github.com/zce/todos.git:gh-pages

--- precache-manifest.c1d58495916a4455c7f9b1b7322cc98b.js
+++ precache-manifest.c1d58495916a4455c7f9b1b7322cc98b.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cf3e82520aaacbab432a127500f91280",
+    "revision": "e4790c974487b566d809060baa55871c",
     "url": "./index.html"
   },
   {
-    "revision": "9b3999fa4ebf5465ccd6",
+    "revision": "469ccbae382db4ac82b8",
     "url": "./static/css/2.8f3b27ef.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/css/main.19b133c6.chunk.css"
   },
   {
-    "revision": "9b3999fa4ebf5465ccd6",
-    "url": "./static/js/2.d7dad587.chunk.js"
+    "revision": "469ccbae382db4ac82b8",
+    "url": "./static/js/2.4e52114e.chunk.js"
   },
   {
     "revision": "7c4dce86fc685d19ee7c",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.7289049684c13442c56a43af32d6c416.js"
+  "./precache-manifest.c1d58495916a4455c7f9b1b7322cc98b.js"
 );
 
 self.addEventListener('message', (event) => {
